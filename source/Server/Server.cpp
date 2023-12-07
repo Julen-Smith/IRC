522ef837c2c0b985cae9b7b534404661f85adb2a
lugonzal <lukas@server.ssh>
2023-12-07 18:10:42 +0000

    CHORE: remote testing

--- source/Server/Server.cpp
+++ source/Server/Server.cpp
@@ -113,7 +113,7 @@ void Server::tokenizer(Message &msg) {
     for (command = msg.commands->begin(); msg.commands->size(); command++) {
         msg.set_params();
         token = msg.get_params_front();
-        std::cout << "Command: " << token << std::endl;
+        //std::cout << "Command: " << token << std::endl;
         this->it = this->callback_map.find(token);
 
         if (this->it != this->callback_map.end())
@@ -122,7 +122,7 @@ void Server::tokenizer(Message &msg) {
             std::cerr << "Contents: " << msg.buffer << std::endl;
             std::cerr << "Error: invalid commnad -> " << token << std::endl;
         }
-        std::cout << "Tokenizer commands size: " << msg.commands->size() << std::endl;
+        //std::cout << "Tokenizer commands size: " << msg.commands->size() << std::endl;
 
         if (msg.params and msg.params->size() != static_cast<size_t>(-1)) {
             delete msg.params;
--- source/main.cpp
+++ source/main.cpp
@@ -32,15 +32,17 @@ void main_loop(Server &server)
                 std::cerr << "main loop delete" << std::endl;
                 server.delete_user_by_socket(server.fds[client].fd);
             }
-            
+
             else if (server.fds[client].revents & POLLIN)  //Comprobación cambios fichero
             {
+                std::cout << "(main loop) - users size: " << server.users.size() << std::endl;
                 std::cout << "(main loop) - fds size: " << server.fds.size() << std::endl;
                 if (server.fds[client].fd == server.get_socket())
                     server.accept_new_user();
                 else
                     server.manage_response(client);
             }
+            std::cout << "(main loop) - users size: " << server.users.size() << std::endl;
         }
     }
 }
